cf161f087fa3729500ea776eb33e2e390285f43d
Nicolas Mendes <nicolasmela312@gmail.com>
2023-01-31 21:14:20 -0300

    temp commit

--- t1/ex1/main.c
+++ t1/ex1/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #include "lista.h"
+#define MAXTAM 100000
 
 int main()
 {
@@ -10,47 +12,74 @@ int main()
     TLista_Inicia(&nova);
 
     // Leitura
-
+    char textoDigitado[MAXTAM], beiju[MAXTAM], naoEhBeiju[MAXTAM];
+    int i;
     char c;
-    c = getchar();
-    while (c != EOF)
+    while(scanf("%c", &c) != EOF)
     {
-        TLista_Insere_Fim(&lista, c);
-        c = getchar();
+        textoDigitado[i] = c;
+        i++;
+    }
+    textoDigitado[i] = '\0';
+    
+    i = 0;
+    
+   // detectando texto beiju
+    int  ehBeiju = 0, iBeiju = 0, iNaoEhBeiju = 0;
+    while (textoDigitado[i] != '\0')
+    {
+        //Verificação se trata-se de um beiju
+        if (textoDigitado[i] == '[')
+        {
+            ehBeiju = 1;
+            i++;
+            
+        }
+        else if (textoDigitado[i] == ']')
+        {
+            ehBeiju = 0;
+            i++;
+        }
+
+        //Adiciona o beiju a string beiju e os demais caracteres vão para a string naoEhBeiju, com exceção dos '[' e ']'
+        if(ehBeiju == 1 && textoDigitado[i] != '[' && textoDigitado[i] != ']')
+        {
+            beiju[iBeiju] = textoDigitado[i];
+            iBeiju++;
+        }
+        else if(ehBeiju == 0 && textoDigitado[i] != '[' && textoDigitado[i] != ']')
+        {
+            naoEhBeiju[iNaoEhBeiju] = textoDigitado[i];
+            iNaoEhBeiju++;
+        }
+
+        //Verificação final
+        if(textoDigitado[i] == ']')
+        {
+            ehBeiju = 0;
+        }
+
+        i++;
+    }
+    //Adicionando \0
+    beiju[iBeiju] = '\0';
+    naoEhBeiju[iNaoEhBeiju] = '\0';
+    printf("%s", textoDigitado);
+   /*
+    //Concatenando as strings
+    strcat(beiju, naoEhBeiju);
+
+    //Adicionando cada elemento da string beiju a lista
+    i = 0;
+    while(beiju[i] != '\0')
+    {
+        TLista_Insere_Fim(&lista, beiju[i]);
+        i++;
     }
 
-    // Processamento
-    TCelula *aux, *aux2;
-    aux2 = nova.cabeca;
-    aux = lista.cabeca->prox;
-    int controle = 0;
-
-    while (aux != NULL)
-    {
-        if (aux->item.letra == '[')
-        {
-            controle = 1;
-            aux = aux->prox;
-        }
-        else if (aux->item.letra == ']')
-        {
-            controle = 0;
-            aux = aux->prox;
-        }
-
-        if(controle == 0 && aux->item.letra != '[' && aux->item.letra != ']')
-        {
-            TLista_Insere_Fim(&nova, aux->item.letra);
-        }else if (controle == 1 && aux->item.letra != '[' && aux->item.letra != ']')
-        {
-            TLista_Insere_Inicio(&nova, aux->item.letra, aux2);
-        }
-
-        aux = aux->prox;
-    }
-    TLista_Imprime(&nova);
-    TLista_Esvazia(&lista);
-    TLista_Esvazia(&nova);
+    //imprime a lista
+    TLista_Imprime(&lista);
+    TLista_Esvazia(&lista);*/
 
     return 0;
 }
