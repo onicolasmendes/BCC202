4233fcf5e88cd92c07abe146f2aa15f407a44c95
Nicolas Mendes <nicolasmela312@gmail.com>
2023-03-09 18:27:15 -0300

    formatting code

--- p9/ordenacao_linear.c
+++ p9/ordenacao_linear.c
@@ -5,13 +5,15 @@
 #include <stdio.h>
 
 // Manter como especificado
-int char2int(char c) {
+int char2int(char c)
+{
     /* Funcao auxiliar para converter um char em um inteiro */
-    return (int) (c - '0');
+    return (int)(c - '0');
 }
 
 // Manter como especificado
-void le(Paciente* pacientes, int n) {
+void le(Paciente *pacientes, int n)
+{
     // PREENCHER AQUI
     /*
      * pacientes = vetor onde devem ser salvos os valores lidos
@@ -20,14 +22,13 @@ void le(Paciente* pacientes, int n) {
     for (int i = 0; i < n; i++)
     {
         scanf("%s %s\n", pacientes[i].senha, pacientes[i].nome);
-        //printf("%s %s\n", pacientes[i].senha, pacientes[i].nome);
-    
+        // printf("%s %s\n", pacientes[i].senha, pacientes[i].nome);
     }
-    
 }
 
 // Manter como especificado
-void imprime(Paciente* pacientes, int n){
+void imprime(Paciente *pacientes, int n)
+{
     // PREENCHER AQUI
     /*
      * pacientes = vetor onde devem ser salvos os valores lidos
@@ -37,11 +38,11 @@ void imprime(Paciente* pacientes, int n){
     {
         printf("%s %s\n", pacientes[i].senha, pacientes[i].nome);
     }
-    
 }
 
 // Manter como especificado
-void coutingSort(Paciente* A, Paciente* B, int n, int i) {
+void coutingSort(Paciente *A, Paciente *B, int n, int i)
+{
     // PREENCHER AQUI
     /*
      * A = vetor a ser ordenado
@@ -50,25 +51,25 @@ void coutingSort(Paciente* A, Paciente* B, int n, int i) {
      * i = digito a ser usado na ordenacao
      */
     int *C = malloc(10 * sizeof(int));
-    for(int j = 0; j < 10; j++)
+    for (int j = 0; j < 10; j++)
     {
         C[j] = 0;
     }
 
-    for(int h = 0; h < n; h++)
+    for (int h = 0; h < n; h++)
     {
         C[char2int(A[h].senha[i])]++;
     }
 
-    for(int l = 1; l < 10; l++)
+    for (int l = 1; l < 10; l++)
     {
-        C[l] += C[l-1];
+        C[l] += C[l - 1];
     }
 
-    for(int j = n - 1; j >= 0; j--)
+    for (int j = n - 1; j >= 0; j--)
     {
-        B[C[char2int(A[j].senha[i])]-1] = A[j];
-        C[char2int(A[j].senha[i])] = C[char2int(A[j].senha[i])] - 1;    
+        B[C[char2int(A[j].senha[i])] - 1] = A[j];
+        C[char2int(A[j].senha[i])] = C[char2int(A[j].senha[i])] - 1;
     }
 
     for (int j = 0; j < n; j++)
@@ -76,13 +77,11 @@ void coutingSort(Paciente* A, Paciente* B, int n, int i) {
         A[j] = B[j];
     }
     free(C);
-    
-
-
 }
 
 // Manter como especificado
-void radixSort(Paciente* pacientes, int n, int m) {
+void radixSort(Paciente *pacientes, int n, int m)
+{
     // Vetor auxiliar a ser usado no couting sort
     Paciente B[MAX_N];
     /*
@@ -94,14 +93,4 @@ void radixSort(Paciente* pacientes, int n, int m) {
     {
         coutingSort(pacientes, B, n, i);
     }
-    
-}
-
-void moveSenha(Paciente *pacientes, int n)
-{
-    for (int i = n; i < 0; i--)
-    {
-        pacientes[i] = pacientes[i-1];   
-    }
-    
 }
